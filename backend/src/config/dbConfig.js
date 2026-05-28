063f12e49a656a6a95c7459fb1a8d66b34df112c
M.Srinivsas shankar <cherry@MSrinivsass-MacBook-Air.local>
2026-05-28 14:45:50 +0530

    db config changed to ipv4

--- backend/src/config/dbConfig.js
+++ backend/src/config/dbConfig.js
@@ -1,7 +1,11 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    connectionString: process.env.DB_CONNECTION_STRING,
+    user:     process.env.DB_USER,
+    host:     process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port:     Number(process.env.DB_PORT) || 5432,
     ssl: {
         rejectUnauthorized: false
     },
